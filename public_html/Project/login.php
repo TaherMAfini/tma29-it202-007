08b039f2489d66142a182dc98ceb807d5bd489d6
TaherMAfini <tma29@njit.edu>
2023-10-17 12:24:36 -0400

    Add logout and home pages

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -4,7 +4,7 @@ require(__DIR__ . "/../../partials/nav.php");
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
-        <input id = "email" type="email" name="email" required />
+        <input type="email" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
@@ -21,48 +21,47 @@ require(__DIR__ . "/../../partials/nav.php");
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
-    //get the email key from $_POST, default to "" if not set, and return the value
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
     $email = se($_POST, "email", "", false);
-    //same as above but for password and confirm
     $password = se($_POST, "password", "", false);
-    $confirm = se($_POST, "confirm", "", false);
-    //TODO 3: validate/use
+
+    //TODO 3
     $hasError = false;
-    if(empty($email)) {
+    if (empty($email)) {
         echo "Email must not be empty";
         $hasError = true;
     }
     //sanitize
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
     //validate
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email address";
         $hasError = true;
     }
-    if(empty($password)) {
-        echo "Password must not be empty";
+    if (empty($password)) {
+        echo "password must not be empty";
         $hasError = true;
     }
-    if(strlen($password) < 8) {
+    if (strlen($password) < 8) {
         echo "Password too short";
         $hasError = true;
     }
-    if(!$hasError) {
-        echo "Welcome, $email";
+    if (!$hasError) {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT email, password from Users WHERE email = :email");
+        $stmt = $db->prepare("SELECT id, email, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
-            if($r) {
+            if ($r) {
                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                if($user) {
+                if ($user) {
                     $hash = $user["password"];
                     unset($user["password"]);
-                    if(password_verify($password, $hash)) {
-                        echo "Welcome $email";
+                    if (password_verify($password, $hash)) {
+                        echo "Weclome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
                     }
@@ -74,5 +73,5 @@ require(__DIR__ . "/../../partials/nav.php");
             echo "<pre>" . var_export($e, true) . "</pre>";
         }
     }
- }
+}
 ?>
