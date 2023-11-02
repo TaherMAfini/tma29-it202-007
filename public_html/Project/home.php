1f10878ab811596d89991cd57adadcae4794090f
TaherMAfini <tma29@njit.edu>
2023-11-02 11:18:08 -0400

    Add js flash messages and additional functions and error handling

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,20 +1,14 @@
 <?php
-require(__DIR__."/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if(is_logged_in()){
-    flash("Welcome, " . get_user_email());
-}
-else{
-    flash("You're not logged in");
-}
-/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- echo "Welcome, " . $_SESSION["user"]["email"]; 
-}
-else{
-  echo "You're not logged in";
-}*/
 
-require(__DIR__."/../../partials/flash.php");
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+?>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
 ?>
