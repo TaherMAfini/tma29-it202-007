4358d3df71e9545bac3a0f7153781602a519f77c
TaherMAfini <tma29@njit.edu>
2023-11-27 15:20:19 -0500

    Add comments explaining constraints

--- public_html/Project/sql/010_alter_table_teams.sql
+++ public_html/Project/sql/010_alter_table_teams.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `Teams` ADD CONSTRAINT `uniqueTeams` UNIQUE (`name`);
+
+-- Add constraint to prevent duplicate team names
--- public_html/Project/sql/011_alter_table_championships.sql
+++ public_html/Project/sql/011_alter_table_championships.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `Championships` ADD CONSTRAINT `uniqueChampionships` UNIQUE (`name`);
+
+-- Add constraint to prevent duplicate championship names
