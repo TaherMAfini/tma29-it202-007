8245cdbe53c39aec88e58fb76efe0d1f1fd414a3
TaherMAfini <tma29@njit.edu>
2023-11-30 21:54:53 -0500

    Add favorites dropdown to nav

---
+++ public_html/Project/favoriteTeams.php
@@ -0,0 +1,13 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+
+?>
+
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
