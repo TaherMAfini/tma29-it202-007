fffca3bbcb896fbad88edc74f4e598c17e3cd262
TaherMAfini <tma29@njit.edu>
2023-12-04 15:59:51 -0500

    Add skeleton for pagination and filter

--- public_html/Project/admin/unassociated_teams.php
+++ public_html/Project/admin/unassociated_teams.php
@@ -7,10 +7,85 @@ if (!has_role("Admin")) {
     die(header("Location: " . get_url("home.php")));
 }
 
+
+$teamFilter = se($_GET, "team", "", false);
+$limit = se($_GET, "limit", 10, false);
+
+if($limit > 100 || $limit < 1) {
+    $limit = 10;
+}
+
+$page = se($_GET, "page", 1, false);
+
+$offset = ($page-1)*$limit;
+
+$total = 1;
+
+$cur_total = 1;
+
+$total_pages = ceil($cur_total/$limit);
+
+function disable_prev($page) {
+    echo $page < 1 ? "disabled" : ""; 
+}
+
+function set_active($page, $i) {
+    echo ($page-1) == $i ? "active" : "";
+}
+
+function disable_next($page) {
+    global $total_pages;
+    echo $page >= $total_pages ? "disabled" : "";
+}
+
+function get_page_url($page) {
+    global $teamFilter;
+    global $limit;
+    $filter_params = [];
+    $filter_params["team"] = se($teamFilter, null, "", false);
+    $filter_params["limit"] = se($limit, null, 10, false);
+    echo http_build_query($filter_params) . "&page=" . $page;
+}
+
 ?>
 
 <div class="container-fluid">
-    <h1>Unassociated Teams</h1>
+    <h1>Unassociated Teams (<?php se($total)?>)</h1>
+
+    <form method="GET" class="list-filter mt-5">
+        <div class="team-filter">
+            <label class="form-label" for="team"><h4>Team</h4></label>
+            <input class="form-control w-50" type="text" name="team" value="<?php se($teamFilter) ?>" />
+        </div>
+        <div class="limit">
+            <label class="form-label" for="limit"><h4>Items/Page (1-100)</h4></label>
+            <input class="form-control w-25" type="number" name="limit" id="limit" value=<?php se($limit) ?>>
+        </div>
+        <?php render_button(["type"=>"submit", "text"=>"Filter"]); ?>
+    </form>
+
+    <form method="GET" class="clear-filter">
+        <div class="clear-filter">
+            <?php render_button(["type"=>"submit", "text"=>"Clear Filter", "color"=>"secondary"]); ?>
+        </div>
+    </form>
+
+    <h3>Items on Page: <?php se($cur_total)?></h3>
+
+    <div>
+        <ul class="pagination justify-content-center">
+            <li class="page-item <?php disable_prev(($page-1))?>">
+                <a class="page-link" href="?<?php get_page_url($page-1); ?>" tabindex="-1">Previous</a>
+            </li>
+            <?php for ($i = 0; $i < $total_pages; $i++) : ?>
+                <li class="page-item <?php set_active($page, $i); ?>"><a class="page-link" href="?<?php get_page_url($i+1) ?>"><?php echo ($i + 1); ?></a></li>
+            <?php endfor; ?>
+            <li class="page-item <?php disable_next(($page)); ?>">
+                <a class="page-link" href="?<?php get_page_url($page+1); ?>">Next</a>
+            </li>
+        </ul>
+    </div>
+
 </div>
 
 <?php
